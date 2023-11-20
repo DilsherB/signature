44c8b46cc9d23a0411a116124781e1c0f8bc6815
Dilsher <dilsherb@gmail.com>
2023-11-20 08:46:13 +0300

    propTypes added.

--- src/projects/components/Title.jsx
+++ src/projects/components/Title.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import PropTypes from 'prop-types'
 
 const Title = ({ classes, text }) => {
   return (
     <div className={classes}>{text}</div>
   )
+}
+
+Title.propTypes = {
+  classes: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
 }
 
 export default Title
