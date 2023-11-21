0200f518db634e12cfabb1b924ee60cdcb60d177
Dilsher <dilsherb@gmail.com>
2023-11-21 13:51:35 +0300

    some infor of the colors added to make it usefull for the user.

--- src/projects/RandomColor/RandomColor.jsx
+++ src/projects/RandomColor/RandomColor.jsx
@@ -1,26 +1,80 @@
-import Title from "../components/Title"
+import React, { useState } from "react";
+import Title from "../components/Title";
 
 const RandomColor = () => {
-  
+  const [fontColor, setFontColor] = useState("#000");
+  const [bgColor, setBgColor] = useState("#fff");
+  const [buttonBgColor, setButtonBgColor] = useState("#007bff");
+  const [buttonColor, setButtonColor] = useState("#fff");
+
   const randomColor = () => {
     const color = Math.floor(Math.random() * 16777215).toString(16);
     return "#" + color;
-  }
+  };
 
   const getRandomColor = (e) => {
-    const color = randomColor();
-    document.body.style.backgroundColor = color;
-    document.body.style.color = color+1;
-    e.target.style.backgroundColor = randomColor()
-  }
+    setFontColor(randomColor());
+    setBgColor(randomColor());
+    setButtonBgColor(randomColor());
+    setButtonColor(randomColor());
+
+    document.body.style.backgroundColor = bgColor;
+    document.body.style.color = fontColor;
+    e.target.style.backgroundColor = buttonBgColor;
+    e.target.style.color = buttonColor;
+
+    const colorInfoElement = document.querySelectorAll("span");
+    colorInfoElement[0].textContent = fontColor;
+    colorInfoElement[1].textContent = bgColor;
+    colorInfoElement[2].textContent = buttonBgColor;
+    colorInfoElement[3].textContent = buttonColor;
+  };
+
   return (
     <div>
-      <Title text={"Randome Color Generation"} classes={"title"}/>
-      <div style={{display: "flex", justifyContent: "center", gap: "5rem"}}>
-        <button className="btnStyle" onClick={getRandomColor}>RANDOM COLOR</button>
+      <Title text={"Random Color Generation"} classes={"title"} />
+      <div style={{ display: "flex", justifyContent: "center", gap: "5rem" }}>
+        <button className="btnStyle" onClick={getRandomColor}>
+          RANDOM COLOR
+        </button>
+      </div>
+      <div
+        style={{
+          border: `1px solid ${buttonBgColor}`,
+          width: "20rem",
+          margin: "1rem auto",
+          textAlign: "left",
+          padding: "1rem",
+        }}
+      >
+        <h2
+          style={{
+            textAlign: "center",
+            textDecoration: "underline",
+            fontSize: "clamp(1rem, 2vw, 2rem)",
+          }}
+        >
+          Colors Information
+        </h2>
+        <div className="colorInfoStyle">
+          <p>Font Color: </p>
+          <p>{fontColor}</p>
+        </div>
+        <div className="colorInfoStyle">
+          <p>Background Color: </p>
+          <p>{bgColor}</p>
+        </div>
+        <div className="colorInfoStyle">
+          <p>Button Background Color: </p>
+          <p>{buttonBgColor}</p>
+        </div>
+        <div className="colorInfoStyle">
+          <p>Button Color: </p>
+          <p>{buttonColor}</p>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default RandomColor
+export default RandomColor;
